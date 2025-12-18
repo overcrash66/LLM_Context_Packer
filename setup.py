f9293aadf0987ed1f0f3c2a497851fd439e68bec
overcrash <3681221+overcrash66@users.noreply.github.com>
2025-12-17 21:46:32 -0400

    init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="llm-context-packer",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "typer[all]>=0.9.0",
